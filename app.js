075b9e2eca1b17f74f13bb206d6fc421ee9163eb
koba789 <kobahide789@gmail.com>
2012-04-17 15:39:26 +0900

    use cluster

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var bouncy = require('bouncy');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-bouncy(function (req, bounce) {
-  bounce(req.headers.host, 80);
-}).listen(80);
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+} else {
+  bouncy(function (req, bounce) {
+    bounce(req.headers.host, 80);
+  }).listen(80);
+}
